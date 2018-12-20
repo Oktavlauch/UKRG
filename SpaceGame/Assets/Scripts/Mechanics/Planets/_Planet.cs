3ab3f0d51cfbb2d1365ab9310fdf3cd07915d8a6
Oktavlauch <46028262+Oktavlauch@users.noreply.github.com>
2018-12-20 19:22:03 +0100

    Update _Planet.cs

--- SpaceGame/Assets/Scripts/Mechanics/Planets/_Planet.cs
+++ SpaceGame/Assets/Scripts/Mechanics/Planets/_Planet.cs
@@ -8,31 +8,38 @@ abstract public class _Planet : MonoBehaviour
     /// Mass of Planet in kg
     /// </summary>
     int Mass;
+
     /// <summary>
     /// Pressure of Planet on Surfacelevel
     /// Surfacelevel = rSurface
     /// </summary>
     int StandardPressure;
+
     /// <summary>
     /// Rotation of Planet around itself
     /// </summary>
     int RotationSpeed;
+
     /// <summary>
     /// r (in Meters) from Centre of Mass to Surfacelevel
     /// </summary>
     int DistanceSurface;
+
     /// <summary>
     /// r (in Meters) between the center of Mass of the Planet to it's Center of Rotation (e.g. Sun)
     /// </summary>
     int DistanceRotatingObject;
+
     /// <summary>
     /// The Speed at which a Planet is going (m/s)
     /// </summary>
     int Speed;
+
     /// <summary>
     /// r (in Meters) from Center of Mass to end of Sphere of Influence
     /// </summary>
     int DistanceSphereOfInfluence;
+
     /// <summary>
     /// The Starting position of the planet at t(0)
     /// May be subject to change
