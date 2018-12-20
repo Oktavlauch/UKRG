5b4f93547b5e63abf69e5641396fa6a749d95467
Simon Thür <thuer.simon@hotmail.com>
2018-12-20 19:20:11 +0100

    Update _Rockets.cs
    Some Variables

--- SpaceGame/Assets/Scripts/Mechanics/Rockets/_Rockets.cs
+++ SpaceGame/Assets/Scripts/Mechanics/Rockets/_Rockets.cs
@@ -2,8 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class _Rockets : MonoBehaviour
+abstract public class _Rockets : MonoBehaviour
 {
+    /// <summary>
+    /// Mass of the Rocket(stage) in kg
+    /// </summary>
+    int Mass;
+
+    /// <summary>
+    /// The current Speed of the rocket in x,y coordinates
+    /// [v]=m/s
+    /// </summary>
+    Vector2 Velocity;
+
+    /// <summary>
+    /// The Rockets current Orientation(0-k*2PI)
+    /// [orientation]=rad
+    /// Orientation Counterclockwise! ; origin(1,0)
+    /// </summary>
+    int Orientation;
+
+    /// <summary>
+    /// The rate at which the orientation changes (angular velocity)
+    /// [omega]=rad/s
+    /// </summary>
+    float AngularVelocity;
+
+    /// <summary>
+    /// The rate at which the Angular Velocity changes (angular accelaration)
+    /// [AngularAccelaration]=rad*s^-2
+    /// </summary>
+    float AngularAcceleration;
+
+    /// <summary>
+    /// The force with which the rocket propels itself (2-Dimensional)
+    /// [F]=m*a
+    /// => a = Force/Mass
+    /// </summary>
+    Vector2 Force;
+
+    /// <summary>
+    /// Current Temperature of the Rocket
+    /// [T]=°C
+    /// </summary>
+    int Temperature;
+
+    /// <summary>
+    /// Critical Temperature at which the rocket will have some serious problems
+    /// [T]=°C
+    /// </summary>
+    int MaxTemperature;
+
+
     // Start is called before the first frame update
     void Start()
     {
