7787d19a0e20c14bce5ca9f3e6f518b7d725cd70
Simon Thür <thuer.simon@hotmail.com>
2018-12-20 20:21:23 +0100

    Update TestRocketGravity.cs
    Oszilliert

--- SpaceGame/Assets/Scripts/Mechanics/Rockets/TestRocketGravity.cs
+++ SpaceGame/Assets/Scripts/Mechanics/Rockets/TestRocketGravity.cs
@@ -13,19 +13,21 @@ public class TestRocketGravity : MonoBehaviour
     /// </summary>
     Vector2 ForceDirection;
     Vector2 ObjectPos = new Vector2(1.0f, 0.0f);
+    Vector2 StartVelocity=new Vector2(0,1);
 
     public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.velocity = StartVelocity;
     }
 
     // Update is called once per frame
     void Update()
     {
         ForceDirection = new Vector2(ObjectPos.x - transform.position.x, ObjectPos.y - transform.position.y);
-
+        ForceValue = 1 / (ForceDirection.magnitude+1);
         rb.AddRelativeForce(ForceDirection.normalized * ForceValue);
     }
 }
