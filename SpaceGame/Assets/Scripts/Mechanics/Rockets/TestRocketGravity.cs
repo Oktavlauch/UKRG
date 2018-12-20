23e142d4e56cd1be17c3f9f1f5e915002482f353
Simon Thür <thuer.simon@hotmail.com>
2018-12-20 20:04:30 +0100

    TestRocketGravity
    TestTest

---
+++ SpaceGame/Assets/Scripts/Mechanics/Rockets/TestRocketGravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRocketGravity : MonoBehaviour
+{
+    /// <summary>
+    /// magnitude of the force
+    /// </summary>
+    float ForceValue=1;
+    /// <summary>
+    /// direction of the force
+    /// </summary>
+    Vector2 ForceDirection;
+    Vector2 ObjectPos = new Vector2(1.0f, 0.0f);
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForceDirection = new Vector2(ObjectPos.x - transform.position.x, ObjectPos.y - transform.position.y);
+
+        rb.AddRelativeForce(ForceDirection.normalized * ForceValue);
+    }
+}
