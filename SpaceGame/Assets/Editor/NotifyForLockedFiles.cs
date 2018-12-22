8afa5bca2fc5ed1a229704323a4bf69aff367cf6
Simon Thür <thuer.simon@hotmail.com>
2018-12-22 21:54:13 +0100

    Unity Hax to lock files
    hell yes, pls dont change shit in the editor folder thx;

---
+++ SpaceGame/Assets/Editor/NotifyForLockedFiles.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class NotifyForLockedFiles: UnityEditor.AssetModificationProcessor
+{
+    public static string[] OnWillSaveAssets(string[] paths)
+    {
+        List<string> pathsToSave = new List<string>();
+
+        for (int i = 0; i < paths.Length; ++i)
+        {
+            FileInfo info = new FileInfo(paths[i]);
+            if (info.IsReadOnly)
+                UnityEditor.EditorUtility.DisplayDialog("Can not save.",
+                "Sorry, but " + paths[i] + " is Read-Only! Please Save as another name.",
+                "Ok");
+            else
+                pathsToSave.Add(paths[i]);
+        }
+
+        return pathsToSave.ToArray();
+    }
+}
