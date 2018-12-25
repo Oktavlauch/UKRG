137ae9ea45f0a9adb2cfc7bb5c179fa510d1b466
Oktavlauch <46028262+Oktavlauch@users.noreply.github.com>
2018-12-26 00:55:16 +0100

    Addes MissionSelection

--- SpaceGame/Assets/Scripts/UI/MainMenu/MainMenuControl.cs
+++ SpaceGame/Assets/Scripts/UI/MainMenu/MainMenuControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuControl : MonoBehaviour
 {
@@ -10,8 +11,21 @@ public class MainMenuControl : MonoBehaviour
     public GameObject MainMenuUI;
     public GameObject OptionsMenuUI;
     public GameObject CareerSelectionUI;
+    public TextMeshProUGUI MissionsText;
+    public string[] Missions = new string[100];
+    public int MissSelected = 0;
 
 
+    void Start()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            Missions[i] = "Mission" + i;
+        }
+        MissionsText.text = Missions[MissSelected];
+    }
+
+    //Load Career Selection
     public void CareerSelection()
     {
         IsInCareer = true;
@@ -19,12 +33,34 @@ public class MainMenuControl : MonoBehaviour
         CareerSelectionUI.SetActive(true);
     }
 
+    //Choose Mission
+    public void MissRight()
+    {
+        if(MissSelected < Missions.Length - 1)
+        {
+            MissionsText.text = Missions[(MissSelected += 1)];
+        }     
+    }
+
+    //Choose Mission
+    public void MissLeft()
+    {
+        if (MissSelected > 0)
+        {
+            MissionsText.text = Missions[MissSelected -= 1];
+        }      
+    }
+
+
+
+    //Load Sandbox
     public void LoadSandbox()
     {
         //SceneManager.LoadScene("SampleScene");
         Debug.Log("Sorry, we don't support Sandbox yet!");
     }
 
+    //Quit Application
     public void QuitGame()
     {
         Application.Quit();
@@ -32,13 +68,15 @@ public class MainMenuControl : MonoBehaviour
 
     void Update()
     {
+        //Exit OptionMenu
         if (Input.GetKeyDown(KeyCode.Escape) && IsInOptions)
         {
             IsInOptions = false;
             OptionsMenuUI.SetActive(false);
             MainMenuUI.SetActive(true);
-        }      
-
+        }     
+        
+        //Exit Career Selection
         if (Input.GetKeyDown(KeyCode.Escape) && IsInCareer)
         {
             IsInCareer = false;
@@ -47,6 +85,7 @@ public class MainMenuControl : MonoBehaviour
         }  
     }
 
+    //Open Options
     public void Options()
     {
         IsInOptions = true;
@@ -56,6 +95,7 @@ public class MainMenuControl : MonoBehaviour
 
     public void Back()
     {
+        //Exit Options
         if (IsInOptions)
         {
             IsInOptions = false;
@@ -63,6 +103,7 @@ public class MainMenuControl : MonoBehaviour
             MainMenuUI.SetActive(true);
         }
 
+        //Exit CareerSelection
         else if (IsInCareer)
         {
             IsInCareer = false;
