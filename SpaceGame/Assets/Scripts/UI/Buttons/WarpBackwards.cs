36a554830e264c2edb88503f6e42ed00411ee163
TheCorypheus <46029484+TheCorypheus@users.noreply.github.com>
2019-09-03 14:24:32 +0200

    A brief history of time

---
+++ SpaceGame/Assets/Scripts/UI/Buttons/WarpBackwards.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpBackwards : MonoBehaviour
+{
+    public void Slower()
+    {
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+}
---
+++ SpaceGame/Assets/Scripts/UI/Buttons/WarpForwards.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpForwards : MonoBehaviour
+{
+    public void Faster()
+        {
+        Time.timeScale = 5f;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+    }
+}
